92940a74c0a55de8ea94e0dd24aecb4fc3fd2e11
Korawich9 <krorawit7818@gmail.com>
2023-11-20 13:43:40 +0700

    Initial commit

---
+++ resources/views/test/ant.blade.php
@@ -0,0 +1,8 @@
+<html>
+    <body>
+        <h1>Gallery of Korawich 65122420009</h1>
+        <p>ant
+        <img src="{{ $ant }}" alt="" width="600px" height="auto"></h1>
+        </p>
+    </body>
+</html>
